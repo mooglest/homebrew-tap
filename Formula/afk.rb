db20597af7d046716bd3093ff6bf8ced386196cb
Luís Serralheiro <encavadas@gmail.com>
2026-03-30 02:20:22 +0100

    afk 0.0.20

--- Formula/afk.rb
+++ Formula/afk.rb
@@ -1,32 +1,32 @@
 class Afk < Formula
   desc "Autonomous Flow Kit daemon and CLI"
   homepage "https://afk.mooglest.com"
-  version "0.0.18"
+  version "0.0.20"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.18/afk-macos-arm64.tar.gz"
+      url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.20/afk-macos-arm64.tar.gz"
       sha256 "6098160ba401d6f95f2bf56fdbfa619dc2fc8164b318bf0a4c96a298676e3f4f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.18/afk-linux-arm64.tar.gz"
+      url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.20/afk-linux-arm64.tar.gz"
       sha256 "0d2aae02b981d22f828934a54e5ab0728a4920909b5094fde70d81b60ddb8d79"
     else
-      url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.18/afk-linux-amd64.tar.gz"
+      url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.20/afk-linux-amd64.tar.gz"
       sha256 "17465fc603e3483a073a2c0fc4ae3b21174c7a97bf128bdb5ad0627e952c9721"
     end
   end
 
   resource "afk-linux-amd64" do
-    url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.18/afk-linux-amd64.tar.gz"
+    url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.20/afk-linux-amd64.tar.gz"
     sha256 "17465fc603e3483a073a2c0fc4ae3b21174c7a97bf128bdb5ad0627e952c9721"
   end
 
   resource "afk-linux-arm64" do
-    url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.18/afk-linux-arm64.tar.gz"
+    url "https://github.com/mooglest/homebrew-tap/releases/download/0.0.20/afk-linux-arm64.tar.gz"
     sha256 "0d2aae02b981d22f828934a54e5ab0728a4920909b5094fde70d81b60ddb8d79"
   end
 
